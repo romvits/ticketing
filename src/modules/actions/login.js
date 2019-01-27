891d93d728d9f468d7d9c3400caaf907c567196e
Roman Marlovits <roman.marlovits@gmail.com>
2019-01-27 15:03:08 +0100

    if wrong login data send evt login-err

--- src/modules/actions/login.js
+++ src/modules/actions/login.js
@@ -6,12 +6,16 @@ class ActionLogin {
 		const conn = settings.conn;
 		const client = settings.client;
 
-		const sql = 'SELECT * FROM t_user WHERE (nickname = ? || email = ?) && password = ?';
+		const sql = 'SELECT firstname, lastname, email, nickname FROM t_user WHERE (nickname = ? || email = ?) && password = ?';
 		const values = [req.username, req.username, req.password];
 
 		conn.query(sql, values, (err, res) => {
 			if (!err) {
-				client.emit('login', res);
+				if (res.length) {
+					client.emit('login', res);
+				} else {
+					client.emit('login-err', {});
+				}
 			}
 			conn.release();
 		});

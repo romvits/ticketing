d258fba0ddd875acd2691f943a6bf62cd7dbc841
Roman Marlovits <marlovits@3binfra.at>
2019-02-11 17:09:40 +0100

    split of list and list columns in database and server logic

--- docker/mysql/sql/t_30_list.sql
+++ docker/mysql/sql/t_30_list.sql
@@ -5,10 +5,27 @@ CREATE TABLE `t_list` (
   `list_id` VARCHAR(32) NOT NULL COMMENT 'unique id of the list - will be a auto generated 32 character string',
   `table` VARCHAR(100) NOT NULL COMMENT 'the name of the table or the view from where the data will be feched',
   `pk` VARCHAR(100) NOT NULL DEFAULT 'id' COMMENT 'which field is the PK field in this table? (default = id) OR (record_id)',
-  `limit` TINYINT(3) NOT NULL DEFAULT 0 COMMENT 'limit of records which will be fechted at once',
-  `editable` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'is this table editable - data are direktly stored to the given table',
+  `limit` TINYINT(3) NOT NULL DEFAULT 50 COMMENT 'limit of records which will be fechted at once',
   `json` JSON NULL COMMENT 'json configuration string for the tabel, information like columns and so on',
   PRIMARY KEY (`list_id`))
 ENGINE = MyISAM DEFAULT CHARSET=UTF8MB4;
 
-INSERT INTO `t_list` (`list_id`,`table`,`pk`,`limit`,`editable`,`json`) VALUES ('mock_data','t_mock_data','id',100,1,'{"columns": [{"name": "gender", "type": "rt_id", "rt_key": "gender", "rt_table": "v_gender", "rt_value": "gender_text"}, {"name": "first_name", "type": "text"}, {"name": "last_name", "type": "text"}], "orderby": "last_name"}');
+INSERT INTO `t_list` (`list_id`,`table`,`pk`,`limit`,`json`) VALUES ('mock_data','t_mock_data','id',100,'{"orderby": "last_name","editable":1}');
+
+DROP TABLE IF EXISTS `t_list_column`;
+CREATE TABLE `t_list_column` (
+  `column_id` VARCHAR(32) NOT NULL COMMENT 'unique id of the list - will be a auto generated 32 character string',
+  `list_id` VARCHAR(32) NOT NULL COMMENT 'id of the list this column is related to',
+  `order` TINYINT(3) NOT NULL DEFAULT 1 COMMENT 'sort order of the column',
+  `name` VARCHAR(100) NOT NULL COMMENT 'the name of the column - must be a field name of "table" from database table t_list',
+  `label` VARCHAR(100) NOT NULL COMMENT 'the name of the column - will be used for translation',
+  `type` VARCHAR(100) NOT NULL COMMENT 'the type of the column',
+  `json` JSON NULL COMMENT 'json configuration string for the column - depends on type of column (eg. rt_id)',
+  PRIMARY KEY (`list_id`, `column_id`))
+ENGINE = MyISAM DEFAULT CHARSET=UTF8MB4;
+
+INSERT INTO `t_list_column` VALUES ('mock_data_col_1','mock_data',1,'gender','§§GENDER','rt','{"rt_key": "gender", "rt_table": "v_gender", "rt_value": "gender_text"}');
+INSERT INTO `t_list_column` VALUES ('mock_data_col_2','mock_data',2,'first_name','§§FIRSTNAME','text','{}');
+INSERT INTO `t_list_column` VALUES ('mock_data_col_3','mock_data',3,'last_name','§§LASTNAME','text','{}');
+
+
--- src/server/actions/list.js
+++ src/server/actions/list.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
 class ActionList {
+
 	constructor(settings) {
 		this._io = settings.io;
 		this._client = settings.client;
@@ -10,87 +11,112 @@ class ActionList {
 	}
 
 	init() {
-		let sql = 'SELECT * FROM t_list WHERE list_id = ?';
-		let values = [this._req.list_id];
+		this.getList(this._req.list_id).then((result) => {
+			this._client.emit('list-init', result);
+			this._db.release();
+		});
+	}
 
-		this._db.query(sql, values, (err, res) => {
-			if (!err) {
-				if (res.length) {
+	getList(list_id, full = false) {
+		var result = {};
+		return new Promise((resolve, reject) => {
+			this._promiseList(list_id, full).then((res) => {
+				result = res;
+				return this._promiseListColumn(list_id);
+			}).then((res) => {
+				result.json.columns = res;
+				resolve(result);
+			}).catch((err) => {
+				console.warn(err);
+				this._db.release();
+			});
+		});
+	}
 
-					let row = res[0];
+	_promiseList(list_id, full = false) {
+		return new Promise((resolve, reject) => {
+			let sql = 'SELECT * FROM t_list WHERE list_id = ?';
+			let values = [list_id];
 
-					sql = 'SELECT COUNT(*) AS count FROM ' + row.table;
+			this._db.query(sql, values, (err, res) => {
+				if (res[0] && !err) {
+
+					sql = 'SELECT COUNT(*) AS count FROM ' + res[0].table;
 					values = [this._req.list_id];
 
 					this._db.query(sql, values, (err, res_count) => {
 						if (!err) {
-
-							let row_count = res_count[0];
-
-							this._client.emit('list-init', {
-								'count': row_count.count,
-								'pk': row.pk,
-								'editable': row.editable,
-								'json': JSON.parse(row.json)
-							});
-							this._db.release();
-						} else {
-							console.warn(err);
-							this._db.release();
+							let result = {
+								'count': res_count[0].count,
+								'pk': res[0].pk,
+								'json': JSON.parse(res[0].json)
+							};
+							if (full) {
+								result.table = res[0].table;
+								result.limit = res[0].limit;
+							}
+							resolve(result);
 						}
 					});
+
 				} else {
-					console.warn(err);
-					this._db.release();
+					reject(err ? err : res);
 				}
-			} else {
-				console.warn(err);
-				this._db.release();
-			}
+			});
+		});
+	}
+
+	_promiseListColumn(list_id) {
+		return new Promise((resolve, reject) => {
+			let sql = 'SELECT * FROM t_list_column WHERE list_id = ? ORDER BY `order`';
+			let values = [list_id];
+
+			this._db.query(sql, values, (err, res) => {
+				if (res && !err) {
+					resolve(res);
+				} else {
+					reject(err ? err : res);
+				}
+			});
 		});
 	}
 
 	fetch() {
-		let sql = 'SELECT * FROM t_list WHERE list_id = ?';
-		let values = [this._req.list_id];
 
-		this._db.query(sql, values, (err, res) => {
-			if (!err) {
-				if (res.length) {
+		this.getList(this._req.list_id, true).then((result) => {
 
-					let row = res[0];
+			let json = result.json;
+			let columns = json.columns;
+			let table = result.table;
+			let limit = result.limit;
+			let fields = result.pk;
+			let orderby = this._req.orderby ? this._req.orderby : json.orderby;
+			let orderdesc = this._req.orderdesc ? this._req.orderdesc : false;
 
-					let json = JSON.parse(row.json);
-					let columns = json.columns;
-					let table = row.table;
-					let limit = row.limit;
-					let fields = row.pk;
-					let orderby = this._req.orderby ? this._req.orderby : json.orderby;
-					let orderdesc = this._req.orderdesc ? this._req.orderdesc : false;
+			_.each(columns, (column) => {
+				fields += ',' + column.name;
+			});
 
-					_.each(columns, (column) => {
-						fields += ',' + column.name;
-					});
+			let sql = 'SELECT ' + fields + ' FROM ' + table;
 
-					sql = 'SELECT ' + fields + ' FROM ' + table;
-					(orderby) ? sql += ' ORDER BY ' + orderby : null;
-					(orderby && orderdesc) ? sql += ' DESC, ' + row.pk : '';
-					(limit) ? sql += ' LIMIT ' + this._req.from + ',' + limit : null;
-
-					values = [];
-					this._db.query(sql, values, (err, rows) => {
-						if (!err) {
-							this._client.emit('list-fetch', {'orderby': orderby, 'rows': rows});
-						} else {
-							console.warn(err);
-						}
-						this._db.release();
-					});
-				}
-			} else {
-				console.warn(err);
-				this._db.release();
+			if (orderby) {
+				sql += ' ORDER BY ' + orderby;
+				(orderdesc) ? sql += ' DESC' : '';
+				sql += ',' + result.pk
 			}
+
+			(limit) ? sql += ' LIMIT ' + this._req.from + ',' + limit : null;
+
+			let values = [];
+			this._db.query(sql, values, (err, rows) => {
+				if (!err) {
+					this._client.emit('list-fetch', {'orderby': orderby, 'rows': rows});
+				} else {
+					console.warn(err);
+				}
+				this._db.release();
+			});
+
 		});
 	}
 }

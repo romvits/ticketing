9a8500fc80bb6f8ec4fdc6a390989c56c7b54da0
Roman Marlovits <roman.marlovits@gmail.com>
2019-04-22 12:51:41 +0200

    build and deploy works OK

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # only for testing will be removed if build process with grunt is implemented correctly
-xcopy ../src/* ../build/docker/node/app/
+mkdir ../build/docker/node/app
+rm -rf ../build/docker/node/app/www
+mkdir ../build/docker/node/app/www
+rm -rf ../build/docker/node/app/server
+mkdir ../build/docker/node/app/server
+cp -r ../src/*.js ../build/docker/node/app/
+cp -r ../src/*.json ../build/docker/node/app/
+cp -r ../src/www/* ../build/docker/node/app/www/
+cp -r ../src/server/* ../build/docker/node/app/server/
 #cd ../build
 #mkdir modules_packaging
 #node_modules/.bin/grunt --gruntfile ./GruntfilePackaging.js
@@ -8,7 +16,7 @@ xcopy ../src/* ../build/docker/node/app/
 
 
 # DOCKER BUILD AND DEPLOY PROCESS
-cd docker
-docker-compose build --force-rm --no-cache
-docker push romarius75/ballkartenonline:ticketing_node
-docker push romarius75/ballkartenonline:ticketing_mysql
+#cd ../build/docker
+#docker-compose build --force-rm --no-cache
+#docker push romarius75/ballkartenonline:ticketing_node
+#docker push romarius75/ballkartenonline:ticketing_mysql

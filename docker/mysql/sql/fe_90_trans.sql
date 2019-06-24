db03201139e99ec3254ac378b3438f63fdae0153
Roman Marlovits <roman.marlovits@gmail.com>
2019-06-24 08:57:00 +0200

    try to bring some order to country and language code tables

--- docker/mysql/sql/fe_90_trans.sql
+++ docker/mysql/sql/fe_90_trans.sql
@@ -16,48 +16,48 @@ INSERT INTO feTransGroup (TransGroupID, TransGroupName) VALUES
 
 DROP TABLE IF EXISTS `feTrans`;
 CREATE TABLE `feTrans` (
-  `TransID` 					VARCHAR(32) DEFAULT NULL COMMENT 'id of parent item (eg EventID, texts and so on)',
+  `TransID` 					VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'id of parent item (eg EventID, TicketID, SeatID)',
   `TransToken` 					VARCHAR(100) NOT NULL COMMENT 'token of the translation',
   `TransLangCode` 				VARCHAR(5) NOT NULL COMMENT 'lang code of the translation',
   `TransTransGroupID` 			VARCHAR(32) NULL COMMENT 'id of trans group',
   `TransValue` 					LONGTEXT COMMENT '',
   FOREIGN KEY TransTransGroupID_TransGroupID (`TransTransGroupID`)   	REFERENCES feTransGroup(`TransGroupID`),
   KEY `group` (`TransLangCode`,`TransTransGroupID`),
-  UNIQUE KEY (`TransID`,`TransToken`,`TransLangCode`)
+  PRIMARY KEY (`TransID`,`TransToken`,`TransLangCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO feTrans (TransToken, TransLangCode, TransTransGroupID, TransValue) VALUES 
+INSERT INTO feTrans (TransID, TransToken, TransLangCode, TransTransGroupID, TransValue) VALUES 
 
-('§§LIST_USER','de','user','Benutzerliste'),
-('§§LIST_MOCKDATA','de','mock_data','Mock Data List'),
+('','§§LIST_USER','de-at','user','Benutzerliste'),
+('','§§LIST_MOCKDATA','de-at','mock_data','Mock Data List'),
 
-('§§USER_GENDER','de',null,'Geschlecht'),
-('§§USER_LASTNAME','de',null,'Nachname'),
-('§§USER_FIRSTNAME','de',null,'Vorname'),
-('§§USER_EMAIL','de',null,'eMail-Adresse'),
-('§§USER_ID','de',null,'User ID'),
-('§§USER_PHONE','de',null,'Telefon'),
-('§§USER_TYPE','de',null,'Art'),
-('§§USER_COMPANY','de',null,'Firma'),
-('§§USER_ORDER_COUNT','de',null,'Extern Best.'),
-('§§USER_ORDER_FROM_COUNT','de',null,'Intern Best.'),
-('§§USER_CREDIT_FROM_COUNT','de',null,'Intern Guts.'),
+('','§§USER_GENDER','de-at',null,'Geschlecht'),
+('','§§USER_LASTNAME','de-at',null,'Nachname'),
+('','§§USER_FIRSTNAME','de-at',null,'Vorname'),
+('','§§USER_EMAIL','de-at',null,'eMail-Adresse'),
+('','§§USER_ID','de-at',null,'User ID'),
+('','§§USER_PHONE','de-at',null,'Telefon'),
+('','§§USER_TYPE','de-at',null,'Art'),
+('','§§USER_COMPANY','de-at',null,'Firma'),
+('','§§USER_ORDER_COUNT','de-at',null,'Extern Best.'),
+('','§§USER_ORDER_FROM_COUNT','de-at',null,'Intern Best.'),
+('','§§USER_CREDIT_FROM_COUNT','de-at',null,'Intern Guts.'),
 
-('§§LIST_USER','en','user','List of users'),
-('§§LIST_MOCKDATA','en','mock_data','Moch Daten Liste'),
+('','§§LIST_USER','en-us','user','List of users'),
+('','§§LIST_MOCKDATA','en-us','mock_data','Moch Daten Liste'),
 
-('§§USER_GENDER','en',null,'Gender'),
-('§§USER_LASTNAME','en',null,'Last name'),
-('§§USER_FIRSTNAME','en',null,'First name'),
-('§§USER_EMAIL','en',null,'eMail-Address'),
-('§§USER_ID','en',null,'User ID'),
-('§§USER_PHONE','en',null,'Phone'),
-('§§USER_TYPE','en',null,'Type'),
-('§§USER_COMPANY','en',null,'Company'),
-('§§USER_ORDER_COUNT','en',null,'Extern Orders'),
-('§§USER_ORDER_FROM_COUNT','en',null,'Intern Orders'),
-('§§USER_CREDIT_FROM_COUNT','en',null,'Intern Credits');
+('','§§USER_GENDER','en-us',null,'Gender'),
+('','§§USER_LASTNAME','en-us',null,'Last name'),
+('','§§USER_FIRSTNAME','en-us',null,'First name'),
+('','§§USER_EMAIL','en-us',null,'eMail-Address'),
+('','§§USER_ID','en-us',null,'User ID'),
+('','§§USER_PHONE','en-us',null,'Phone'),
+('','§§USER_TYPE','en-us',null,'Type'),
+('','§§USER_COMPANY','en-us',null,'Company'),
+('','§§USER_ORDER_COUNT','en-us',null,'Extern Orders'),
+('','§§USER_ORDER_FROM_COUNT','en-us',null,'Intern Orders'),
+('','§§USER_CREDIT_FROM_COUNT','en-us',null,'Intern Credits');
 
 

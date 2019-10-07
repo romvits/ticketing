f843dbecd28e3c566f89bf1905ab50a6d73e4303
Roman Marlovits <roman.marlovits@gmail.com>
2019-10-07 19:54:42 +0200

    some adjustments for special scripts

--- _stuff/jart/action.js
+++ _stuff/jart/action.js
@@ -5,47 +5,70 @@
 
 function startAction() {
 
-	$('#TAB_home').click();
-
-	this.blockTablesAnz = 1000;
-	$ajapp.order.activateSelling();
+	let min = 1000;
+	let max = 5000;
 
 	window.setTimeout(() => {
-		this.$ajapp.order.goNextTab();
-		this.$ajapp.order.wk.sitze_anz = 99999;
-	}, 1000);
+		$('#TAB_home').click();
 
-	window.setTimeout(() => {
-		let tab = $('#siztzanz');
-		let selectAnz = tab.find('.form-control.ck0');
-		selectAnz.append('<option value="' + this.$ajapp.order.wk.sitze_anz + '">' + this.$ajapp.order.wk.sitze_anz + '</option>');
-		selectAnz.val(this.$ajapp.order.wk.sitze_anz);
-		this.$ajapp.order.kartenSitzeChanged(null, this.$ajapp.order.wk.sitze_anz);
-	}, 2000);
+		this.blockTablesAnz = 99999;
+		this.blockSeatsAnz = 200;
+		$ajapp.order.activateSelling();
 
-	window.setTimeout(() => {
-		$('.seatNextBtn').click();
-		//this.$ajapp.order.goNextTab();
-	}, 3000);
+		window.setTimeout(() => {
+			this.$ajapp.order.goNextTab();
+			this.$ajapp.order.wk.sitze_anz = 99999;
+		}, 1000);
 
-	window.setTimeout(() => {
-		let tables = $.find('.tischEl');
-		let sleeper = 5000;
-		let countTables = 0;
-		tables.forEach((table) => {
-			if (countTables <= this.blockTablesAnz) {
-				window.setTimeout(() => {
-					console.log('addToWk', table);
-					$ajapp.plan.addToWk($(table));
-				}, sleeper);
-				sleeper = sleeper + 1000;
-			}
-			countTables++;
-		});
-	}, 1000);
+		window.setTimeout(() => {
+			let tab = $('#siztzanz');
+			let selectAnz = tab.find('.form-control.ck0');
+			selectAnz.append('<option value="' + this.$ajapp.order.wk.sitze_anz + '">' + this.$ajapp.order.wk.sitze_anz + '</option>');
+			selectAnz.val(this.$ajapp.order.wk.sitze_anz);
+			this.$ajapp.order.kartenSitzeChanged(null, this.$ajapp.order.wk.sitze_anz);
+		}, 2000);
+
+		window.setTimeout(() => {
+			$('.seatNextBtn').click();
+		}, 3000);
+
+		window.setTimeout(() => {
+			let tablesAction = [];
+			let tables = $.find('.tischEl');
+			let sleeper = 5000;
+			let countTables = 0;
+			let countSeats = 0;
+			tables.forEach((table) => {
+				let $table = $(table);
+				let seats = $table.find('.seatFree');
+				if (parseInt(seats.length) > 4) {
+					tablesAction.push({
+						$table: $table,
+						table: table,
+						seats: parseInt(seats.length)
+					});
+				}
+			});
+			tablesAction.forEach((table) => {
+
+				let x = (Math.random() * (max - min)) + min;
+
+				if (countSeats < this.blockSeatsAnz) {
+					window.setTimeout(() => {
+						console.log('addToWk', x, table);
+						$ajapp.plan.addToWk(table.$table);
+					}, sleeper);
+					sleeper = sleeper + x;
+					countSeats = countSeats + table.seats;
+				}
+			});
+			console.log(countSeats);
+		}, 1000);
+	}, 5000);
 }
 
 startAction();
+
 // look at this.$ajapp.order.wk
 
 
@@ -55,7 +78,7 @@ function startAction() {
 
 	$('#tabs').tabs("select", 0);
 
-	let blockTablesAnz = 5;
+	let blockTablesAnz = 50;
 	let countTables = 0;
 
 	let selectAnz = $('#asplatz');
